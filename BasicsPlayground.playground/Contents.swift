62c3b110f2e36f10740aa2473aaaa323e4778457
Lukasz <>
2023-06-05 20:28:33 +0200

    For stride to/through

--- BasicsPlayground.playground/Contents.swift
+++ BasicsPlayground.playground/Contents.swift
@@ -16,3 +16,11 @@ print(ints) // [Optional(5), Optional(7), nil]
 var intsWithoutNil = numbers.compactMap { Int($0) }
 print(intsWithoutNil) // [5, 7]
 
+var numbersArray = Array(0...100)
+for i in numbersArray {
+    print(i)
+}
+
+for number in stride(from: 0, through: 12, by: 2) {
+    print(number)
+}
